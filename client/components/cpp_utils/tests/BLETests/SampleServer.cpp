2efa0d078a366f9ee0f4a08db388a1f2087759ec
Ellison Yasukochi <smellison@gmail.com>
2017-11-26 20:15:13 -0800

    Created client project. Tested flashing these two and it might be working but the logging isn't registering for some reason despite the fact that I can see other debug level logs

--- client/components/cpp_utils/tests/BLETests/SampleServer.cpp
+++ client/components/cpp_utils/tests/BLETests/SampleServer.cpp
@@ -1,11 +1,6 @@
-/*
- * 1. Open up the project properties
- * 2. Visit C/C++ General > Preprocessor Include Paths, Macros, etc
- * 3. Select the Providers tab
- * 4. Check the box for "CDT GCC Built-in Compiler Settings"
- * 5. Set the compiler spec command to "xtensa-esp32-elf-gcc ${FLAGS} -E -P -v -dD "${INPUTS}""
- * 6. Rebuild the index
-*/
+/**
+ * Create a new BLE server.
+ */
 #include "BLEDevice.h"
 #include "BLEServer.h"
 #include "BLEUtils.h"
@@ -17,14 +12,7 @@
 
 #include "sdkconfig.h"
 
-
-//static char tag[]="cpp_helloworld";
 static char LOG_TAG[] = "SampleServer";
-
-extern "C" {
-	void app_main(void);
-}
-
 
 class MainBLEServer: public Task {
 	void run(void *data) {
@@ -33,10 +21,10 @@ class MainBLEServer: public Task {
 		BLEDevice::init("ESP32");
 		BLEServer* pServer = BLEDevice::createServer();
 
-		BLEService* pService = pServer->createService("6d124ed1-50f5-4ebf-b490-c3db81cbaa8c");
+		BLEService* pService = pServer->createService("91bad492-b950-4226-aa2b-4ede9fa42f59");
 
 		BLECharacteristic* pCharacteristic = pService->createCharacteristic(
-			BLEUUID("4c7a3456-6ac2-4e16-9951-028dc32c443c"),
+			BLEUUID("0d563a58-196a-48ce-ace2-dfec78acc814"),
 			BLECharacteristic::PROPERTY_BROADCAST | BLECharacteristic::PROPERTY_READ  |
 			BLECharacteristic::PROPERTY_NOTIFY    | BLECharacteristic::PROPERTY_WRITE |
 			BLECharacteristic::PROPERTY_INDICATE
@@ -59,34 +47,12 @@ class MainBLEServer: public Task {
 	}
 };
 
-//class Greeting {
-//public:
-//	void helloEnglish() {
-//		ESP_LOGD(tag, "Hello %s", name.c_str());
-//	}
-//
-//	void helloFrench() {
-//		ESP_LOGD(tag, "Bonjour %s", name.c_str());
-//	}
-//
-//	void setName(std::string name) {
-//		this->name = name;
-//	}
-//private:
-//	std::string name = "";
-//
-//};
 
-void app_main(void)
+void SampleServer(void)
 {
-//	Greeting myGreeting;
-//	myGreeting.setName("Neil");
-//	myGreeting.helloEnglish();
-//	myGreeting.helloFrench();
-
 	//esp_log_level_set("*", ESP_LOG_DEBUG);
 	MainBLEServer* pMainBleServer = new MainBLEServer();
 	pMainBleServer->setStackSize(20000);
 	pMainBleServer->start();
-}
 
+} // app_main
